02372e074ba61d5cb4ac6dfadc6993c1a27f7983
Jason Smith <jason@smithss.org>
2017-08-05 11:46:14 -0700

    initial

---
+++ variables.tf
@@ -0,0 +1,39 @@
+##General vars
+variable "ssh_user" {
+  default = "ubuntu"
+}
+variable "public_key_path" {
+  default = "/Users/jruels/.ssh/id_rsa.pub"
+}
+variable "private_key_path" {
+  default = "/Users/jruels/.ssh/id_rsa"
+}
+##AWS Specific Vars
+variable "aws_worker_count" {
+  default = 2
+}
+variable "aws_key_name" {
+  default = "jruels"
+}
+variable "aws_instance_size" {
+  default = "t2.micro"
+}
+variable "aws_region" {
+  default = "us-west-1"
+}
+##GCE Specific Vars
+variable "gce_worker_count" {
+  default = 1
+}
+variable "gce_creds_path" {
+  default = "/Users/spencer/gce-creds.json"
+}
+variable "gce_project" {
+  default = "test-project"
+}
+variable "gce_region" {
+  default = "us-central1"
+}
+variable "gce_instance_size" {
+  default = "n1-standard-1"
+}
