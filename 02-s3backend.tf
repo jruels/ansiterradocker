692022e2fd6925f4ec38db06803e9a3e74a4f376
Jason Smith <jason@smithss.org>
2017-08-28 18:17:25 -0700

    Halo bucket

--- 02-s3backend.tf
+++ 02-s3backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "jenkins-tf"
-    key    = "dev/terraform.tfstate"
+    bucket = "halo-tf"
+    key    = "test/terraform.tfstate"
     region = "us-east-1"
   }
 }
